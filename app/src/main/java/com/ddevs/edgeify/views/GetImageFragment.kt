dcc20fd74537c3e3a9d99afcdccd6a7018b1d0b5
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2022-09-12 14:41:36 +0530

    Feat: pick and capture image

--- app/src/main/java/com/ddevs/edgeify/views/GetImageFragment.kt
+++ app/src/main/java/com/ddevs/edgeify/views/GetImageFragment.kt
@@ -1,20 +1,47 @@
 package com.ddevs.edgeify.views
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.ddevs.edgeify.R
+import com.ddevs.edgeify.databinding.FragmentGetImageBinding
+import java.io.File
 
 
 class GetImageFragment : Fragment() {
-
+    lateinit var binding:FragmentGetImageBinding
+    val imagePic = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){
+        Log.e("Camerapic",it.toString());
+    }
+    val captureImage = registerForActivityResult(ActivityResultContracts.TakePicture()){
+        Log.e("Camera",it.toString());
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_image, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_get_image, container, false)
+        binding.pickBtn.setOnClickListener {
+            imagePic.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        binding.captureBtn.setOnClickListener {
+            val file = File(requireContext().filesDir, "images/image.jpeg")
+            val uri: Uri = FileProvider.getUriForFile(
+                requireContext(),
+                requireActivity().applicationContext.packageName + ".provider",
+                file
+            )
+            captureImage.launch(uri)
+        }
+        return binding.root
     }
 }
